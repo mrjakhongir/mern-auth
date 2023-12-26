a9026e4db9c7998c912cf534291d24298c8ca6cd
Jahongir_Nusratov <nusratovjahongir@gmail.com>
2023-12-26 15:53:32 +0500

    feat: connect to MONGODB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose
+  .connect(
+    'mongodb+srv://Nusratov_Jahongir:jahongir96@mern.ahgse9p.mongodb.net/mern-auth?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to MONGODB')
+    app.listen(3000, () => {
+      console.log('App is running on port: 3000')
+    })
+  })
+  .catch((err) => console.log(err))
 
 const app = express()
-
-app.listen(3000, () => {
-  console.log('App is running on port: 3000')
-})
