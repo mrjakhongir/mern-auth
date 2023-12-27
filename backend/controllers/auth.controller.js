8b83c5a0d08e455c63d5ec485ce65afb335b0719
Jahongir_Nusratov <nusratovjahongir@gmail.com>
2023-12-27 11:28:01 +0500

    feat: add bcryptjs for convert in encrypted password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,16 @@
 import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body
-  const newUser = new User({ username, email, password })
-  await newUser.save()
-  res.status(201).json({ message: 'response sended' })
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+  const newUser = new User({ username, email, password: hashedPassword })
+  try {
+    await newUser.save()
+    res.status(201).json({ message: 'response sended' })
+  } catch {
+    ;(err) => {
+      res.status(500).json(err.message)
+    }
+  }
 }
