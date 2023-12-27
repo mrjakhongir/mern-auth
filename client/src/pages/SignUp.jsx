6365c70dd254b5480ef0c7696a2c58bee142971c
Jahongir_Nusratov <nusratovjahongir@gmail.com>
2023-12-27 12:03:43 +0500

    feat: customize signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,43 @@
+import { Link } from 'react-router-dom'
+
 export default function SignUp() {
-  return <div>SignUp</div>
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4 ">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          autoComplete="off"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          autoComplete="off"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          SIGN UP
+        </button>
+        {/* <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          CONTINUE WITH GOOGLE
+        </button> */}
+      </form>
+      <p className="mt-5">
+        Have an account?{' '}
+        <Link className="text-blue-500" to="/sign-in">
+          Sign in
+        </Link>
+      </p>
+    </div>
+  )
 }
