39714a45cefe0c45eaba9c9cac8e150973d8bcee
mrjakhongir <nusratovjahongir@gmail.com>
2024-08-12 12:50:05 +0500

    feat: create signin api route.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export async function signupController(req, res, next) {
 	const { username, email, password } = req.body;
@@ -9,6 +11,31 @@ export async function signupController(req, res, next) {
 		await newUser.save();
 		res.status(201).json({ message: 'User created successfully' });
 	} catch (err) {
-		next();
+		next(err);
+	}
+}
+
+export async function signinController(req, res, next) {
+	const { email, password } = req.body;
+	try {
+		const validUser = await User.findOne({ email });
+		if (!validUser) {
+			return next(errorHandler(404, 'Incorrect email or password'));
+		}
+
+		const validPassword = bcryptjs.compareSync(password, validUser.password);
+		if (!validPassword) {
+			return next(errorHandler(401, 'Incorrect email or password'));
+		}
+
+		const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+		const { password: hashedPassword, ...user } = validUser._doc;
+		const expityDate = new Date(Date.now() + 86400000);
+		res
+			.cookie('access_token', token, { httpOnly: true, expires: expityDate })
+			.status(200)
+			.json(user);
+	} catch (err) {
+		next(err);
 	}
 }
